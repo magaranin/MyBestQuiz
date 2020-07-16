c537b91fbcb10b197f3087e58afb185ca5258db0
Margarita Sirbu <margaritapetru@hotmail.com>
2020-07-16 15:04:13 -0700

    It will make changes to my js and make sure that the options are consider when giving a recommendation

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,36 @@ $(document).ready(function () {
     $(".hideShow").first().css("display", "block");
 
     $(document).on("click", ".next", function() {
+        const age = parseInt($("#age").val());
+        if (isNaN(age) || 
+            ($(this).closest('.options').find("input[type=radio]").length > 0 &&
+             $(this).closest('.options').find("input:checked").length === 0) {
+            alert("Please answer the question.");
+            return;
+        }
         $(this).closest('.hideShow').hide().next().show();
     })
 
-    $("#bestRecommendation").submit(function (event) {
-        alert();
-        const age = parseInt($("#age").val());
-        const gender = $("#gender").val();
-        const drink = $("#drink").val();
-        const strenghts = $("#strenghts").val();
-        const knowledge = $("#knowledge").val();
-        const temperament = $("#temperament").val();
-        const personality = $("#personality").val();
+    $('input[name=gender]:checked', '#bestRecommendation').val();
 
+    $("#bestRecommendation").submit(function (event) {
+        
+        const age = parseInt($("#age").val());
+        const gender = $('input[name=gender]:checked', '#bestRecommendation').val();
+        const drink = $('input[name=drink]:checked', '#bestRecommendation').val();
+        const strenghts = $('input[name=strenghts]:checked', '#bestRecommendation').val();
+        const subject = $('input[name=subject]:checked', '#bestRecommendation').val();
+        const temperament = $('input[name=temperament]:checked', '#bestRecommendation').val();
+        const personality = $('input[name=personality]:checked', '#bestRecommendation').val();
+        alert(gender);
         let ourRecommendation = "Python";
-        if (isNaN(age)) {
-            alert('Please enter your age.');
-            return;
-        }
 
         if (gender === 'male' &&
             age < 35  &&
             (drink === 'alcohol' || 
             drink === 'coffee' ||
             strenghts === 'problem-solving' ||
-            knowledge === 'geometry' ||
+            subject === 'geometry' ||
             temperament === 'melancholic' ||
             personality === 'director')) {
                ourRecommendation = "C++";
@@ -36,7 +41,7 @@ $(document).ready(function () {
             (drink ==='water' ||
             drink ==='tea'||
             strenghts === 'attention to detail' ||
-            knowledge === 'algebra' ||
+            subject === 'algebra' ||
             temperament === 'phlegmatic' ||
             personality === 'thinker')) {
                 ourRecommendation= "Python";
@@ -46,7 +51,7 @@ $(document).ready(function () {
             (drink ==='coffee' ||
             drink ==='tea' ||
             strenghts === 'logical thinking'||
-            knowledge === 'history' ||
+            subject === 'history' ||
             temperament === 'choleric' ||
             personality === 'socializer')) {
                 ourRecommendation = "HTML & CSS";
@@ -56,7 +61,7 @@ $(document).ready(function () {
             (drink === 'alcohol' ||
             drink === 'water' ||
             strenghts === 'work well on a team' ||
-            knowledge === 'design' ||
+            subject === 'design' ||
             temperament === 'sanguine' ||
             personality === 'supporter')) {
                 ourRecommendation ="Ruby";
